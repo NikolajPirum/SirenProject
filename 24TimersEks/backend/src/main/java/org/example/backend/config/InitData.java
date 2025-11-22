5dcc1a8fcdf1afefb5025b9d62d0821e1bb30903
nikolajpirum <nikolajpirum.10@gmail.com>
2025-11-22 14:09:05 +0100

    added h2

--- 24TimersEks/backend/src/main/java/org/example/backend/config/InitData.java
+++ 24TimersEks/backend/src/main/java/org/example/backend/config/InitData.java
@@ -10,7 +10,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
-import java.util.List;
+
 
 @Component
 public class InitData implements CommandLineRunner {
